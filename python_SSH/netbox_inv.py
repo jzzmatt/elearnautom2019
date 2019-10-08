1f4c9ceb99fdbb5668aede1649f09df5a6df4c5e
Junior Mateus <mateusjunior.ns@gmail.com>
2019-10-08 16:16:46 +0100

    add site to netbox

--- python_SSH/netbox_inv.py
+++ python_SSH/netbox_inv.py
@@ -16,6 +16,7 @@ NETBOX_RESSOURCES = {
     'interfaces': '/dcim/interfaces',
 }
 
+
 TOKEN = '91e9482beba078638f98f936bdbf8b745bcd0dce'
 HEADERS = {
     'Authorization': 'Token {}'.format(TOKEN),
@@ -23,7 +24,16 @@ HEADERS = {
     'Accept': 'application/json'
 }
 
+SITES = [
+    {'name': 'CABINDA', 'slug': 'cab'},
+    {'name': 'LUBANGO', 'slug': 'lub'},
+ 
+]
+
 def netbox_query(resources, query_params=None):
+    '''
+    Perform a Simple Query to Netbox via Device API
+    '''
     return requests.get(
         NETBOX_URL + NETBOX_RESSOURCES[resources], params=query_params, headers=HEADERS
     )
@@ -31,3 +41,34 @@ def netbox_query(resources, query_params=None):
 nbx_devices = netbox_query('devices')
 
 print(nbx_devices)
+
+def nbx_add_site(name='', slug=None):
+    '''
+    Manualy Add a Device from a Yaml Inventory to Netbox API 
+    '''
+    data = {
+        'name' : name,
+        'slug' : slug,
+    }
+    req = requests.post(NETBOX_URL + NETBOX_RESSOURCES['sites'], headers=HEADERS, json=data)
+    if req.status_code == 201:
+        #201 => mean sucess
+        print(f'==>Site {name} was created Successfully')
+    else:
+        req.raise_for_status()
+
+
+def push_sites_to_api():
+    '''
+    Add sites from Sites Dictionnary to NetBOX API
+    '''
+    for site in SITES:
+        nbx_add_site(**site)
+
+
+def main():
+    push_sites_to_api()
+
+if __name__ == '__main__':
+    main()
+
